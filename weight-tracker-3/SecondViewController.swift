31f4600eb3d4c6cb4896590611e3706911ad1fef
Ben Noland <ben.noland@gmail.com>
2018-10-29 08:53:50 -0600

    Initial Commit

---
+++ weight-tracker-3/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  weight-tracker-3
+//
+//  Created by Ben Noland on 10/29/18.
+//  Copyright © 2018 Ben Noland. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
